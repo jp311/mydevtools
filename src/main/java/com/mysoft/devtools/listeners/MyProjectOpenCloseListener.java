2c7d3a316f77acbbda131535e9da0691a8bd743f
何章笛 <hezd@mingyuanyun.com>
2023-05-26 00:54:07 +0800

    增加检查：new Service、Resource注解字段名称规范检查

--- src/main/java/com/mysoft/devtools/listeners/MyProjectOpenCloseListener.java
+++ src/main/java/com/mysoft/devtools/listeners/MyProjectOpenCloseListener.java
@@ -2,21 +2,22 @@ package com.mysoft.devtools.listeners;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
+import com.mysoft.devtools.Inspections.OverrideInspection;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author hezd 2023/4/24
  */
 public class MyProjectOpenCloseListener implements ProjectManagerListener {
-//    /**
-//     * 打开项目事件（已过时，后期版本可能会删除）
-//     *
-//     * @param project
-//     */
-//    @Override
-//    public void projectOpened(@NotNull Project project) {
-//        System.out.println("project opend!");
-//    }
+    /**
+     * 打开项目事件（已过时，后期版本可能会删除）
+     *
+     * @param project
+     */
+    @Override
+    public void projectOpened(@NotNull Project project) {
+        OverrideInspection.doOverride(project);
+    }
 
     /**
      * 关闭项目事件
