01a8237c6b8a3d7da3803a8dcac3dd3e1734960f
何章笛 <hezd@mingyuanyun.com>
2023-07-09 22:39:37 +0800

    bug fix

--- src/main/java/com/mysoft/devtools/utils/idea/psi/PsiModuleExtension.java
+++ src/main/java/com/mysoft/devtools/utils/idea/psi/PsiModuleExtension.java
@@ -1,5 +1,6 @@
 package com.mysoft.devtools.utils.idea.psi;
 
+import com.intellij.ide.projectView.actions.MarkSourceRootAction;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.ModifiableRootModel;
@@ -7,6 +8,7 @@ import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiManager;
+import org.jetbrains.jps.model.java.JavaSourceRootType;
 
 /**
  * @author hezd   2023/7/9
@@ -23,9 +25,14 @@ public class PsiModuleExtension {
             if (psiBaseDir != null) {
                 testDir = PsiDirectoryExtension.createSubdirectorys(psiBaseDir, "src/test/java");
                 baseDir.refresh(true, true);
+
+                //将目录设置单测源代码目录，否则代码不能正常识别
+                VirtualFile virtualFile = testDir.getVirtualFile();
+                ContentEntry entry = MarkSourceRootAction.findContentEntry(modifiableModel, virtualFile);
+                entry.addSourceFolder(virtualFile, JavaSourceRootType.TEST_SOURCE);
             }
         }
-
+        modifiableModel.commit();
         modifiableModel.dispose();
         return testDir;
     }
